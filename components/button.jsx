711172ab05f511f6b1037d1c849a387c089ea10e
denosaurabh <saurabh.fullstackdev@outlook.com>
2021-06-11 15:21:22 +0530

    Delete components directory

--- components/button.jsx
+++
@@ -1,9 +0,0 @@
-import { button } from '@styles/components/button.module.css'
-
-const Button = ({children, onClickHandler}) => {
-    return (
-        <button className={button} onClick={onClickHandler}>{children}</button>
-    )
-}
-
-export default Button;
